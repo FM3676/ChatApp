4358af73df8487886c985292d65bfd4a1484f51d
§ <1309346392@qq.com>
2022-11-25 01:02:16 +0800

    Finish Chat Input Basic Input Feature

--- client/src/components/ChatContainer.tsx
+++ client/src/components/ChatContainer.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { User } from "../types";
+import ChatInput from "./ChatInput";
 import Logout from "./Logout";
+import Message from "./Message";
 interface ChatContainerProps {
   currentChat: User;
 }
 export default function ChatContainer({ currentChat }: ChatContainerProps) {
+  const handleSendMsg = async (msg:any) => {}
   return (
-    <div className="pt-4">
+    <div className="grid gap-1 h-full overflow-hidden" style={{gridTemplateRows:"1fr 6fr 1fr"}}>
       <div className="flex justify-between items-center px-8">
         <div className="flex items-center gap-4">
           <div>
@@ -24,8 +27,8 @@ export default function ChatContainer({ currentChat }: ChatContainerProps) {
         </div>
         <Logout />
       </div>
-      <div></div>
-      <div></div>
+      <Message />
+      <ChatInput handleSendMsg={handleSendMsg} />
     </div>
   );
 }
---
+++ client/src/components/ChatInput.tsx
@@ -0,0 +1,66 @@
+import { FormEvent, MouseEvent, useState } from "react";
+import Picker, { IEmojiData } from "emoji-picker-react";
+import { IoMdSend } from "react-icons/io";
+import { BsEmojiSmileFill } from "react-icons/bs";
+import "./ChatInput.scss";
+
+interface ChatInputInterface {
+  handleSendMsg: Function;
+}
+
+export default function ChatInput({ handleSendMsg }: ChatInputInterface) {
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+  const [msg, setMsg] = useState("");
+
+  const handleEmojiPickerHideShow = () => setShowEmojiPicker(!showEmojiPicker);
+
+  const handleEmojiClick = (
+    event: MouseEvent<Element, globalThis.MouseEvent>,
+    data: IEmojiData
+  ) => {
+    let message = msg + data.emoji;
+    setMsg(message);
+  };
+
+  const sendChat = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (msg.length === 0) return;
+    handleSendMsg(msg);
+    setMsg("");
+  };
+  return (
+    <div
+      className="grid items-center px-8 pb-1"
+      style={{ backgroundColor: "#080420", gridTemplateColumns: "5% 95%" }}
+    >
+      <div className="flex items-center text-white gap-4">
+        <div className="emoji">
+          <BsEmojiSmileFill
+            className="text-2xl cursor-pointer"
+            style={{ color: "#ffff00c8" }}
+            onClick={handleEmojiPickerHideShow}
+          />
+          {showEmojiPicker && <Picker onEmojiClick={handleEmojiClick} />}
+        </div>
+      </div>
+      <form
+        className="w-full flex items-center gap-8 rounded-3xl bg-gray-700"
+        onSubmit={(e) => sendChat(e)}
+      >
+        <input
+          className="h-3/5 w-11/12 bg-transparent border-none text-white pl-4 text-xl selection:bg-indigo-500 focus:outline-none"
+          type="text"
+          placeholder="Type your message."
+          onChange={(e) => setMsg(e.target.value)}
+          value={msg}
+        />
+        <button
+          className="py-1 px-8 flex justify-center items-center bg-indigo-500 border-none"
+          type="submit"
+        >
+          <IoMdSend className="text-3xl text-white" />
+        </button>
+      </form>
+    </div>
+  );
+}
